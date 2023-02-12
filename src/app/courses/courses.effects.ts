57d9f91e6dd4a18dca783d5fcc210e19675add00
Chesire <emmanuelchesire1@gmail.com>
2023-02-12 07:03:08 +0300

    update courses using ngrx

--- src/app/courses/courses.effects.ts
+++ src/app/courses/courses.effects.ts
@@ -10,7 +10,15 @@ export class CoursesEffects {
 	loadCourses$ = createEffect(() => this.actions$.pipe(
 		ofType(CourseActions.loadAllCourses),
 		concatMap(action => this.coursesHttpService.findAllCourses()),
-		map(courses => allCoursesLoaded({courses}))))
+		map(courses => allCoursesLoaded({courses}))));
+
+	saveCourse$ = createEffect(() => this.actions$.pipe(
+		ofType(CourseActions.courseUpdated),
+		concatMap(action => this.coursesHttpService.saveCourse(
+			action.update.id,
+			action.update.changes
+		))
+	), {dispatch: false})
 	constructor(
 		private actions$: Actions,
 		private coursesHttpService: CoursesHttpService) {
--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -21,7 +21,9 @@ export const coursesReducer = createReducer(
 			...state,
 			allCoursesLoaded: true
 		}
-	))
+	)),
+
+	on(CourseActions.courseUpdated, (state, action) => adapter.updateOne(action.update, state))
 )
 
 export const {selectAll} = adapter.getSelectors()
